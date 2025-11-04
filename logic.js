fff5330a68829532cf7ab051f0a70bc41b4f9f8b
Himanshu-K06 <himanshu952511@gmail.com>
2025-11-04 12:13:45 +0530

    add logic for human choice

--- logic.js
+++ logic.js
@@ -1,1 +1,16 @@
 console.log("WELCOME! TO THE GAME OF ROCK PAPER AND SCISSORS....");
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let num = Math.random();
+    if(num>=0 && num<0.4) return "Rock";
+    else if(num>=0.4 && num<0.7) return "Paper";
+    else return "Scissors";
+}
+
+function getHumanChoice(){
+    let choice = prompt("Choose one Rock/Paper/Scissors :");
+    console.log(`user chose ${choice}`);
+}
