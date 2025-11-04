d26431abfa510361a93ec0e1a6f7b7aed26c32aa
Himanshu-K06 <himanshu952511@gmail.com>
2025-11-04 12:32:52 +0530

    write round playing function and logic

--- logic.js
+++ logic.js
@@ -1,16 +1,43 @@
-console.log("WELCOME! TO THE GAME OF ROCK PAPER AND SCISSORS....");
+console.log("WELCOME! TO THE GAME OF ROCK PAPER AND SCISSOR....");
 
 let humanScore = 0;
 let computerScore = 0;
 
 function getComputerChoice(){
     let num = Math.random();
-    if(num>=0 && num<0.4) return "Rock";
-    else if(num>=0.4 && num<0.7) return "Paper";
-    else return "Scissors";
+    if(num>=0 && num<0.4) return "rock";
+    else if(num>=0.4 && num<0.7) return "paper";
+    else return "scissor";
 }
 
 function getHumanChoice(){
-    let choice = prompt("Choose one Rock/Paper/Scissors :");
-    console.log(`user chose ${choice}`);
+    let choice = prompt("Choose one Rock/Paper/Scissor :");
+    return choice;
 }
+
+function playRound(humanChoice,computerChoice){
+    humanChoice = getHumanChoice().toLowerCase();
+    computerChoice = getComputerChoice();
+    if(humanChoice === "rock" && computerChoice === "paper"){
+        console.log(`You Lose!, ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+    else if(humanChoice === "paper" && computerChoice === "scissor"){
+        console.log(`You Lose!, ${computerChoice} beats ${humanChoice}`);
+        computerScore++;
+    }
+    else if(humanChoice === "scissor" && computerChoice === "rock"){
+        console.log(`You Lose!, ${computerChoice} beats ${humanChoice} `);
+        computerScore++;
+    }
+    else if(humanChoice===computerChoice){
+        console.log(`It's a tie!! ${humanChoice} and ${computerChoice}`);
+    }
+    else{
+        console.log(`You Won !! ${humanChoice} beats ${computerChoice}`);
+        humanScore++;
+    }
+    console.log(`Your Score : ${humanScore} , Computer Score : ${computerScore}`);
+}
+
+playRound();
